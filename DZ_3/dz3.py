42481669297b4a687a1a6e7e27a3a6d358d45948
Georgiy Lelikov <jet_biker@mail.ru>
2022-10-29 18:43:11 +0300

    DZ 3, task 2

--- DZ_3/dz3.py
+++ DZ_3/dz3.py
@@ -10,21 +10,27 @@
 # 2. Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 # Пример: [2, 3, 4, 5, 6] => [12, 15, 16];
 #         [2, 3, 5, 6] => [12, 15]
-# list1 = [2, 3, 4, 5, 6, 7, 8] # не решена верно!!
+# list1 = [2, 3, 4, 5, 6]
 # print(f'Список: {list1}')
 # list2 = []
-# list3 = []
-# for i in range(0, len(list1) // 2):
-#     if i == 0:
-#         list2 = list1[i]*list1[-1]
-#     if i != 0 and len(list1) % 2 == 0:
-#         list2 = list1[i]*list1[-(i+1)]
-#     if i != 0 and len(list1) % 2 != 0:
-#         list2 = list1[i]*list1[-(i+1)]
-#         list3 = (len(list1)-1)**2
-#     print(list2)
-# print(list3) # не решена верно!!
-
+# if len(list1) % 2 == 0:
+#     for i in range(0, len(list1) // 2):
+#         if i == 0:
+#             prod1 = list1[i]*list1[-1]
+#             list2.append(prod1)
+#         else:
+#             prod1 = list1[i]*list1[-(i+1)]
+#             list2.append(prod1)
+#     print(f'Произведение пар чисел {list2}')
+# else:
+#     for i in range(0, (len(list1)+1) // 2):
+#         if i == 0:
+#             prod1 = list1[i]*list1[-1]
+#             list2.append(prod1)
+#         else:
+#             prod1 = list1[i]*list1[-(i+1)]
+#             list2.append(prod1)
+#     print(f'Произведение пар чисел {list2}')
 
 # 3. Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 # Пример: [1.1, 1.2, 3.1, 5, 10.01] => 0.19
